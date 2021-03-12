e07adf4fc97a51091c726abfa1d65c2553f9e6f3
Julie J <jeannet.julie@gmail.com>
2021-03-12 19:30:47 +0100

    Etat d' l'utilisateur observation avec AngularFireAuth et création d'un bouton de déconnexion

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+
 // ne pas oublier
 import firebase from 'firebase/app';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,23 +11,39 @@ import firebase from 'firebase/app';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  result:any;
+  result: any;
+  user: firebase.User | any;
 
-  constructor(private afAuth:AngularFireAuth) { }
+  constructor(private afAuth: AngularFireAuth) { }
 
   ngOnInit(): void {
+    // il faut connaitre l'état d'authentification ou non de l'utilisateur
+    // au moment de l'initialisation du component
+    // pour pouvoir éventuellement le déconnecter
+    // grâce à l'instance d'AngularFireAuth
+    // qui nous retourne un observable
+    // auquel il nous suffit de nous abonner !
+    this.afAuth.authState.subscribe(user => {
+      // on récupère la valeur courante : soit un user, soit null
+      this.user = user;
+    });
+
   }
 
-  async loginWithGoogle(){
+  async loginWithGoogle() {
 
     try {
       var provider = new firebase.auth.GoogleAuthProvider();
-      this.result = await this.afAuth.signInWithPopup(provider) ;
+      this.result = await this.afAuth.signInWithPopup(provider);
 
     } catch (error) {
 
     }
 
+  }
+
+  async signOut() {
+    await this.afAuth.signOut();
   }
 
 }
