2b6a1cc733f9a969e79f17f7352a38a21e5e024b
Julie J <jeannet.julie@gmail.com>
2021-03-12 16:17:30 +0100

    S'authentifier à l'aide d'un compte Gmail via un GoogleAuthProvider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // firebase
+// import firebase from "firebase";
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuthModule } from "@angular/fire/auth";
 import { environment } from "../environments/environment";
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+// ne pas oublier
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  result:any;
 
-  constructor() { }
+  constructor(private afAuth:AngularFireAuth) { }
 
   ngOnInit(): void {
+  }
+
+  async loginWithGoogle(){
+
+    try {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      this.result = await this.afAuth.signInWithPopup(provider) ;
+
+    } catch (error) {
+
+    }
+
   }
 
 }
